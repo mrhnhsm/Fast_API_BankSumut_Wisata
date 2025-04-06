4c5f59b7f00eab0243e8400ac7d03bb69785fff3
M. RAIHAN EFDIN HASYIM <83999229+mrhnhsm@users.noreply.github.com>
2025-04-06 09:28:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.include_router(products.router, prefix="/products", tags=["Products"])
 def home():
     return {"message": "Welcome To API Bank Sumut"}
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     port = int(os.environ.get("PORT", 8000))
-#     uvicorn.run("main:app", host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
